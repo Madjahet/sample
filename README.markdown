dc9663e17cdb56dfcec5a4e6d16b399e073a389c
Madjahet <madjahet88@gmail.com>
2012-05-06 14:19:32 +0300

    Первая фиксация.

---
+++ README.markdown
@@ -0,0 +1,5 @@
+# Ruby on Rails Tutorial: пример приложения
+
+Это учебное приложение, созданное по книге [Майкла Хартла](http://michaelhartl.com/).
+[*"Ruby on Rails Tutorial: Learn Rails by Example"*](http://railstutorial.org/)
+ 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = '6fb52c2ea60c5808938985ed93e4a6a2e8cfcad430cff9c3f2fe3cd910b23b712c529118f5370014bf4c43e87f7bda5bcd91615f8344eec4bf46a8e6b884f13b'
