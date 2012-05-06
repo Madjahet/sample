840a2cee33a8f8e2ca0c894ec8ae61225a831072
Madjahet <madjahet88@gmail.com>
2012-05-07 00:06:46 +0300

    Изучение шаблонов и маршрутизации.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Sample::Application.routes.draw do
-  get "pages/home"
-  get "pages/contact"
-	get "pages/about"
+  get "users/new"
+
+	match '/signup', :to => 'users#new'
+
+  root :to => 'pages#home'
+  match '/contact', :to => 'pages#contact'
+	match '/about', :to => 'pages#about'
+	match '/help', :to => 'pages#help'
 end
